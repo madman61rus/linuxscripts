c4a9bf44f98d1c0cc12f0c25fbbb3b8c42018e25
Vladimir Kompaniec <vkompaniec@gmail.com>
2015-10-29 13:41:18 +0300

    + Убрал из списка корневой каталог + Добавил вывод ошибки - "Файл не найден"

--- check_recursive_file.py
+++ check_recursive_file.py
@@ -71,41 +71,46 @@ if __name__ == '__main__':
     listOfDirs = []
     listWithout = []
 
-    for dirName in os.walk(unicode(parameters['where'], 'utf-8')):
+    for dirName in os.walk(parameters['where']):
 
-        parameters['allCounter'] += 1
+        if not parameters['where'] == dirName[0]:
 
-        if parameters['name'] in dirName[2]:
+            parameters['allCounter'] += 1
 
-            file = {
-                'pathToFile': dirName[0],
-                'filesInPath': dirName[2],
-                'timeOfFile': datetime.fromtimestamp(
-                        getmtime(dirName[0] + "/" + parameters['name'])),
-                'sizeOfFile': getsize(dirName[0] + "/" + parameters['name']),
-            }
-            parameters['counter'] += 1
-            listOfDirs.append(file)
+            if filter(lambda str: parameters['name'] == str, dirName[2]):
 
-            if parameters['lastTimeOfFile'] < file['timeOfFile']:
-                parameters['lastTimeOfFile'] = file['timeOfFile']
-            if parameters['sizeCounter'] < file['sizeOfFile']:
-                parameters['sizeCounter'] = file['sizeOfFile']
-        else:
+                file = {
+                    'pathToFile': dirName[0],
+                    'filesInPath': dirName[2],
+                    'timeOfFile': datetime.fromtimestamp(
+                         getmtime(dirName[0] + "/" + parameters['name'])),
+                    'sizeOfFile': getsize(dirName[0]+"/"+parameters['name']),
+                }
+                parameters['counter'] += 1
+                listOfDirs.append(file)
 
-            listWithout.append(dirName[0])
+                if parameters['lastTimeOfFile'] < file['timeOfFile']:
+                    parameters['lastTimeOfFile'] = file['timeOfFile']
+                if parameters['sizeCounter'] < file['sizeOfFile']:
+                    parameters['sizeCounter'] = file['sizeOfFile']
+            else:
+
+                listWithout.append(dirName[0])
+
+    if (parameters['counter'] == 0):
+        print "ALERT ! The file %s not found" % parameters['name']
+        sys.exit(2)
 
     if (parameters['allContent'] == 'True') and not \
        (parameters['allCounter'] == parameters['counter']):
         print 'WARNING !!! In '+"and".join(listWithout)+" file is not exist"
         sys.exit(1)
-
-    if (((datetime.now()
-       - parameters['lastTimeOfFile']).days) > parameters['time']):
-        print "ALERT ! The last file %s was written %i day(s) ago." % \
-            ("".join(parameters['name']),
-             (datetime.now() - parameters['lastTimeOfFile']).days)
-        sys.exit(2)
+    elif (((datetime.now()
+          - parameters['lastTimeOfFile']).days) > parameters['time']):
+                print "ALERT ! The last file %s was written %i day(s) ago." % \
+                 ("".join(parameters['name']),
+                  (datetime.now() - parameters['lastTimeOfFile']).days)
+                sys.exit(2)
     elif (parameters['sizeCounter'] < parameters['size']):
         print "WARNING ! The size of file is less then %s" % \
            parameters['sizeStr']
